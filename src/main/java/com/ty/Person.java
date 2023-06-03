204bdd907634d994144abace9a7129ac93aefd71
bhoomikatn <bhoomikankanchan@gmail.com>
2023-06-03 12:37:08 +0530

    person

---
+++ src/main/java/com/ty/Person.java
@@ -0,0 +1,46 @@
+package com.ty;
+
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+
+
+@Entity
+public class Person 
+{  @Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+   private String name;
+   private String adress;
+   private long phone;
+   
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+public long getPhone() {
+	return phone;
+}
+public void setPhone(long phone) {
+	this.phone = phone;
+}
+@Override
+public String toString() {
+	return "Person [id=" + id + ", name=" + name + ", adress=" + adress + ", phone=" + phone + "]";
+}
+   
+}
